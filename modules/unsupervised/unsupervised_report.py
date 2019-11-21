8236a09054e9e074b5ae852e229609685927286b
Simen Næss <simen.naess@hotmail.com>
2019-11-21 21:06:26 +0000

    Patternmining implemented in unsupervised_report. Complete function for moshaak. Lacks plots of bins, and corr between support, avg quality of rules and numb of patterns found

--- modules/unsupervised/unsupervised_report.py
+++ modules/unsupervised/unsupervised_report.py
@@ -1,3 +1,5 @@
+from modules.unsupervised.patternmining.apriori import patternmining_ct, patternmining_pd
+
 from modules.unsupervised.clustering.clustering import kmeans_Cluster_pd
 from modules.unsupervised.clustering.clustering import kmeans_Cluster_covtype
 
@@ -5,6 +7,19 @@ PLOT = False
 
 
 def unsupervised_report(data, source):
+    df=data.copy()
+
+    print("1. Applies Pattern Mining & Rules Assosiation:")
+    if source == "PD":
+        print("K=35, Min_patterns=300, Discretization with qcut with bins=6")
+        patternmining_pd(df)
+
+    elif source == "CT":
+        print("K=25, Min_patterns=300, Discretization with cut with bins=4")
+        patternmining_ct(df)
+
+
+
     print("")
     print("Clustering 4.2")
     if source == "PD":
